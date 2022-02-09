8cca08c4e5832ec623965a1e673df4d12427a29a
JimmyZea <jimmy.zea@uptc.edu.co>
2022-02-09 11:55:37 -0500

    fix(Root): Change useHistory to useNavigate from de latest version of react-router-dom

--- src/components/login/UserVerification.js
+++ src/components/login/UserVerification.js
@@ -1,11 +1,11 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 export const UserContext = createContext('user-context');
 
 const ValidateUser = ({ children }) => {
-    const history = useHistory();
+    const history = useNavigate();
 
     const [user, setUser] = useState();
     const [showChild, setShowChild] = useState(false);
@@ -18,28 +18,31 @@ const ValidateUser = ({ children }) => {
             setUser(JSON.parse(user));
             setShowChild(true);
         } else {
-            history.push('/login');
+            setTimeout(() => {
+                history.push('/login');
+            }, 500);
         }
 
         return () => {
             setUser(null);
         }
-        
+
     }, [history]);
 
     if (showChild) {
         return (
-          <UserContext.Provider value={{ user }}>
-            {children}
-          </UserContext.Provider>);
+            <UserContext.Provider value={{ user }}>
+                {children}
+            </UserContext.Provider>);
     } else {
+
         return <>No tiene permisos</>;
     }
 };
 
 ValidateUser.propTypes = {
     children: PropTypes.oneOfType(
-      [PropTypes.object, PropTypes.arrayOf(PropTypes.element)]).isRequired,
-  };
+        [PropTypes.object, PropTypes.arrayOf(PropTypes.element)]).isRequired,
+};
 
 export default ValidateUser;
