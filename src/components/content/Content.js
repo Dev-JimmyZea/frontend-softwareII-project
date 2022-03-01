8d9c7e05fb775f98f8e2273daf1f4084c319153d
Esteban-Duarte31 <yessidduarte7@gmail.com>
2022-03-01 13:00:22 -0500

    feat(Root): Load and show works of backend and change URL for key in .env

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -3,31 +3,35 @@ import './content.css'
 import Slider from 'react-slick';
 import { useEffect, useState } from 'react';
 
-export default function Content () {
+export default function Content({ isNews }) {
 
-    const URL = 'http://localhost:4000/news';
     const [news, setNews] = useState([]);
+    const [work, setWork] = useState([]);
 
     const handleClick = (id) => {
-        console.log(id);
-        if(localStorage.getItem('token') === null){
+        if (localStorage.getItem('token') === null) {
             console.log('No estas logeado');
             window.location.href = '/login';
 
-        }else{
-            window.location.href = `/news/${id}`;
+        } else {
+            window.location.href = isNews ? `/news/${id}` : `/work/${id}`;
             console.log('Estas logeado');
         }
     }
 
     useEffect(() => {
-        fetch(URL)
-            .then(response => response.json())
-            .then(data => {
-                setNews(data.data);
-            })
-            .catch(error => console.log(error));    
-        }, []);
+        console.log(process.env);
+        isNews ?
+            fetch(`${process.env.REACT_APP_URL_BACKEND}news`)
+                .then(res => res.json())
+                .then(data => setNews(data.data))
+                .catch(err => console.log(err))
+            :
+            fetch(`${process.env.REACT_APP_URL_BACKEND}work`)
+                .then(res => res.json())
+                .then(data => setWork(data.data))
+                .catch(err => console.log(err))
+    },[isNews])
 
     const settings = {
         dots: true,
@@ -64,20 +68,34 @@ export default function Content () {
     return (
         <div className={'content-container'}>
             <div className={'content-cards'}>
-                <h1 className={'main-title'}>Noticias</h1>
+                <h1 className={'main-title'}>{isNews ? 'Noticias' : 'Trabajos'}</h1>
                 <Slider {...settings} id={'slider'}>
-                    {news.map(news => (
-                        <Cards
-                            key={news.code}
-                            code={news.code}
-                            title={news.title}
-                            content={news.content}
-                            created_at={news.created_at.substring(0, 10)}
-                            handleClick={handleClick}
-                            image={news.image}
-                            isNews={true}
-                        />
-                    ))}
+                    {
+                        isNews ?
+                            news.map(news => (
+                                <Cards
+                                    key={news.code}
+                                    code={news.code}
+                                    title={news.title}
+                                    content={news.content}
+                                    created_at={news.created_at.substring(0, 10)}
+                                    handleClick={handleClick}
+                                    image={news.image}
+                                />
+                            ))
+                            :
+                            work.map(work => (
+                                <Cards
+                                    key={work.code}
+                                    code={work.code}
+                                    title={work.title}
+                                    content={work.description}
+                                    created_at={work.created_at.substring(0, 10)}
+                                    handleClick={handleClick}
+                                    image={work.image}
+                                />
+                            ))
+                    }
 
                 </Slider>
             </div>
