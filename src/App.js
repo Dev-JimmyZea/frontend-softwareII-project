bbccd2b9b429b4dafe61662716eb7037ec498ecb
Esteban-Duarte31 <yessidduarte7@gmail.com>
2022-03-01 12:56:26 -0500

    feat(Root): Routes for news, work and forum is added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Home from "./components/sites/Home";
+import Forum from "./components/sites/Forum";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
@@ -10,6 +11,10 @@ const App = () => (
     <Routes>
       <Route exact path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
+      <Route path="/news/:id" element={<Home />} />
+      <Route path="/work" element={<Home />} />
+      <Route path="/work/:id" element={<Home />} />
+      <Route path="/forum" element={<Forum />} />
     </Routes>
   </Router>
 );
