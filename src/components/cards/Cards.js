aabdfb012b8e42cdbdf09f31acc3fa81624ad7c7
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-01 21:36:28 -0500

    feat(Root): Add a tag for redirect one work or one news and adjust structure

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -3,21 +3,24 @@ import './cards.css';
 const Cards = (props) => {
     return <>
         <div className={'cards-container'} >
-            <div className={'content-card'} onClick={() => props.handleClick(props.code)}>
-                <div className={'content-image'}>
-                    <img src={props.image} alt={'image_news'} className={'image'} />
-                </div>
-                <div className={'content-card-text'}>
-                    <p>{props.content}</p>
-                </div>
-                <div className={'content-card-date'}>
-                    <span>{props.created_at}</span>   
-                </div>
-                <div className={'content-card-button'}>
-                    <button className={'button-card'} >Ver más</button>
-                </div>
+            <a href={`./${props.site}/${props.code}`}>
+                <div className={'content-card'}>
 
-            </div>
+                    <div className={'content-image'}>
+                        <img src={props.image} alt={'image_news'} className={'image'} />
+                    </div>
+                    <div className={'content-card-text'}>
+                        <p>{props.content}</p>
+                    </div>
+                    <div className={'content-card-date'}>
+                        <span>{props.created_at}</span>
+                    </div>
+                    <div className={'content-card-button'}>
+                        <button className={'button-card'} href={`./news/${props.code}`}>Ver más</button>
+                    </div>
+
+                </div>
+            </a>
         </div>
     </>
 };
