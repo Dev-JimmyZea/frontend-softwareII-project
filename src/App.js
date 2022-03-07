d107e46ce8ff6f5c6cd4358a2941ddfea0af6244
Esteban-Duarte31 <yessidduarte7@gmail.com>
2022-03-07 07:02:55 -0500

    feat(Root): Delete validateUser from routes and add routes for superadmin

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Home from "./components/sites/Home";
 import ListForums from "./components/sites/ListForums";
-import ValidateUser from "./components/login/UserVerification";
 import News from "./components/sites/News";
-
+import Users from "./components/sites/Users";
+import Careers from "./components/sites/Careers";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route exact path="/login" element={<Login />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/news/:id" element={<ValidateUser><News /></ValidateUser>} />
-      <Route path="/work" element={<Home />} />
-      <Route path="/work/:id" element={<ValidateUser><Home /></ValidateUser>} />
-      <Route path="/forum" element={<ListForums />} />
-      <Route path="/forum/:code" element={<ValidateUser><ListForums /></ValidateUser>} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login pathname={window.location.pathname} />} />
+      <Route exact path="/news/:id" element={<News />} />
+      <Route exact path="/work" element={<Home />} />
+      <Route exact path="/forum" element={<ListForums />} />
+      <Route exact path="/superadmin-users" element={<Users />} />
+      <Route exact path="/superadmin-careers" element={<Careers />} />
     </Routes>
   </Router>
 );
