f0a5f1ffa980e470abb31d4e4d35ca1901e397d8
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-08 18:44:11 -0500

    feat(Root): Add sweet alerts for data messages

--- src/components/sites/Item.js
+++ src/components/sites/Item.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Swal from 'sweetalert2'
 import Navbar from "../navbar/Navbar"
 import Footer from "../footer/Footer"
 import ItemComponent from "../Item/Item"
@@ -6,10 +7,10 @@ import ValidateUser from "../login/UserVerification"
 
 
 const Item = ({ object }) => {
+
     const id = window.location.pathname.split('/')[2];
     const [data, setData] = useState([]);
     const user = localStorage.getItem('user');
-    console.log(user);
 
     const getData = async (url, meth) => {
         const response = await fetch(url, {
@@ -29,27 +30,84 @@ const Item = ({ object }) => {
 
     useEffect(() => {
         getData(`${process.env.REACT_APP_URL_BACKEND}${object}/${id}`, 'GET')
-            .then(data => setData(data.data))
+            .then(data => {
+                if (data.message === 'New fetched successfully' || data.message === 'Work fetched successfully') {
+                    setData(data.data);
+                } else {
+                    window.history.back();
+                }
+            }
+            )
             .catch(err => console.log(err))
-    }, [id, object, user]);
+    }, [id, object]);
+
+    const apply = async (idStudent, no_apply) => {
+        Swal.fire({
+            title: !no_apply ? 'Deseas aplicar a este trabajo?' : 'Desea cancelar la aplicación?',
+            showCancelButton: true,
+            confirmButtonText: 'Si',
+            cancelButtonText: 'Cancelar',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                getData(`${process.env.REACT_APP_URL_BACKEND}work/${no_apply ? 'remove/' : '/'}${id}/${idStudent}`, 'PUT')
+                    .then(data => {
+                        if (data.message === 'Applicant added successfully') {
+                            Swal.fire({
+                                position: 'top',
+                                icon: 'success',
+                                title: 'Has aplicado exitosamente a este trabajo',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        } else if (data.message === 'Applicant removed successfully') {
+                            Swal.fire({
+                                position: 'top',
+                                icon: 'success',
+                                title: 'Has cancelado la aplicación exitosamente a este trabajo',
+                                showConfirmButton: false,
+                                timer: 1500
+                            })
+                        }
+                        setData(data.data)
+                    })
+                    .catch(err => console.log(err))
+            } else {
+                Swal.fire({
+                    position: 'top',
+                    icon: 'error',
+                    title: 'Cancelado',
+                    showConfirmButton: false,
+                    timer: 1000
+                })
+            }
+
+        })
+    }
 
     return (
         <>
             <ValidateUser role='ALL'>
                 <Navbar />
-                <ItemComponent 
-                    object={object} 
-                    title={data.title}
-                    charge={data.charge}
-                    content={object === 'news' ? data.content : data.description}
-                    image={data.image}
-                    created_at={data.created_at}
-                    salary={data.base_salary}
-                    company={data.company}
-                    applicants={data.applicants}
-                    user={user ? JSON.parse(user) : null}
-                    valid_until={data.valid_until}
-                />
+                {
+                    data ?
+                        <ItemComponent
+                            object={object}
+                            title={data.title}
+                            charge={data.charge}
+                            content={data.content || data.description}
+                            image={data.image}
+                            created_at={data.created_at}
+                            salary={data.base_salary}
+                            company={data.company}
+                            applicants={data.applicants}
+                            user={user ? JSON.parse(user) : null}
+                            valid_until={data.valid_until}
+                            apply={apply}
+                            id={1}
+                        />
+                        :
+                        null
+                }
                 <Footer />
             </ValidateUser>
         </>)
