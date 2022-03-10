68fc4fbbfc4a8228acc1291c5cb70c99583a6128
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-09 21:15:57 -0500

    feat(Root): Put aleatory bacground image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/login/Login";
-import Home from "./components/sites/Home";
-import ListForums from "./components/sites/ListForums";
-import Item from "./components/sites/Item";
-import Users from "./components/sites/Users";
-import Careers from "./components/sites/Careers";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React, { useEffect } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./components/login/Login"
+import Home from "./components/sites/Home"
+import ListForums from "./components/sites/ListForums"
+import Item from "./components/sites/Item"
+import Users from "./components/sites/Users"
+import Careers from "./components/sites/Careers"
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const App = () => {
 
 
   useEffect(() => {
-    const random = Math.floor(Math.random() * 15) + 1;
-    const url = `https://www.uptc.edu.co/sitio/export/sites/default/portal/.galleries/gal_fon_por/2sem_2021/fot_port_uptc_0${random}.jpg`;
-    const bodyContainer = document.getElementById('body-container');
+    const token = localStorage.getItem('token')
+    const expired = token ? JSON.parse(atob(token.split('.')[1])).exp < Date.now() / 1000 : false
+    
+    if(expired){
+      localStorage.clear()
+    }
+
+    const random = Math.floor(Math.random() * 15) + 1
+    const url = `https://www.uptc.edu.co/sitio/export/sites/default/portal/.galleries/gal_fon_por/2sem_2021/fot_port_uptc_0${random}.jpg`
+    const bodyContainer = document.getElementById('body-container')
     if (bodyContainer) {
       bodyContainer.style.background = `url(${url}) no-repeat center center fixed`
       bodyContainer.style.backgroundSize = 'cover'
@@ -34,12 +41,11 @@ const App = () => {
         <Route exact path="/forum" element={<ListForums />} />
         <Route exact path="/superadmin-users" element={<Users />} />
         <Route exact path="/superadmin-careers" element={<Careers />} />
-        {/* // redirect to home if no match */}
         <Route path="*" element={<Home no_match={true} />} />
 
       </Routes>
     </Router>
   )
-};
+}
 
-export default App;
+export default App
