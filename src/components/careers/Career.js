60e375dc0e9a24bd05bef3b3bc7e7302cea41efc
Esteban-Duarte31 <yessidduarte7@gmail.com>
2022-03-13 00:06:14 -0500

    feat(Root): Import crud component, create application form to object and send props

--- src/components/careers/Career.js
+++ src/components/careers/Career.js
@@ -1,10 +1,70 @@
+import Crud from '../crud/Crud'
 import './career.css'
 
 const Career = () => {
 
+  const form =
+    <div className={'container-form-crud'}>
+      <div className="form-group">
+        <label htmlFor="code">Codigo</label>
+        <input type="text" className="form-control" name="code" id="code" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Nombre</label>
+        <input type="text" className="form-control" name="name" id="name" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="faculty">Facultad</label>
+        <select className="form-control" id="faculty" name="faculty" required>
+          <option defaultValue="Seleccione" disabled selected>Seleccione una facultad</option>
+          <option value="Ciencias Agropecuarias">Ciencias Agropecuarias</option>
+          <option value="Ciencias">Ciencias</option>
+          <option value="Ciencias de la Educación">Ciencias de la Educación</option>
+          <option value="Ciencias Económicas y Administrativas">Ciencias Económicas y Administrativas</option>
+          <option value="Ciencias de la Salud">Ciencias de la Salud</option>
+          <option value="Derecho y Ciencias Sociales">Derecho y Ciencias Sociales</option>
+          <option value="Ingeniería">Ingenería</option>
+          <option value="Estudios a Distancia">Estudios a Distancia</option>
+          <option value="Seccional Chiquinquirá">Seccional Chiquinquirá</option>
+          <option value="Seccional Duitama">Seccional Duitama</option>
+          <option value="Seccional Sogamoso">Seccional Sogamoso</option>
+        </select>
+      </div>
+
+    </div>
+
   return (
-    <div className={'user-container'}>Career</div>
+    <div className={'career-container'}>
+      <div className={'career-title'}>
+        <h1>Carreras</h1>
+      </div>
+      <div className={'career-content'}>
+        <Crud
+          title={'Carreras'}
+          object={'Career'}
+          columns={[
+            {
+              name: 'Código',
+              key: 'code',
+              type: 'text'
+            },
+            {
+              name: 'Nombre',
+              key: 'name',
+              type: 'text'
+            },
+            {
+              name: 'Facultad',
+              key: 'faculty',
+              type: 'text'
+            },
+          ]}
+          form={form}
+        />
+      </div>
+    </div>
   )
 }
+
 
 export default Career
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,9 +1,122 @@
+import { useState, useEffect } from 'react'
+import Crud from '../crud/Crud'
 import './user.css'
 
 const User = () => {
 
+  const [careers, setCareers] = useState([])
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_URL_BACKEND}career`)
+      .then(res => res.json())
+      .then(data => setCareers(data.data))
+      .catch(err => console.log(err))
+
+  }, [])
+
+  const form =
+    <div className={'container-form-crud'}>
+      <div className="form-group">
+        <label htmlFor="id">Id Usuario</label>
+        <input type="text" className="form-control" name="userId" id="id" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="name">Nombre</label>
+        <input type="text" className="form-control" name="name" id="name" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="lastName">Apellido</label>
+        <input type="text" className="form-control" name="lastName" id="lastName" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input type="email" className="form-control" name="email" id="email" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" className="form-control" name="password" id="password" required />
+      </div>
+      <div className="form-group">
+        <label htmlFor="career">Carrera</label>
+        <select className="form-control" id="career" name="career" required>
+          <option defaultValue="Seleccione" disabled selected>Seleccione una carrera</option>
+          {
+            careers.map(career => (
+              <option key={career._id} value={career._id}>{career.name}</option>
+            ))
+          }
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="role">Rol</label>
+        <select className="form-control" name="role" id="role" required>
+          <option defaultValue="Seleccione" disabled selected>Seleccione un rol</option>
+          <option value="ADMIN">Administrador</option>
+          <option value="STUDENT">Estudiante</option>
+        </select>
+      </div>
+      <div className="form-group">
+        <label htmlFor="gender">Género</label>
+        <select className="form-control" name="gender" id="gender" required>
+          <option defaultValue="Seleccione" disabled selected>Seleccione un geénero</option>
+          <option value="M">Masculino</option>
+          <option value="F">Femenino</option>
+        </select>
+      </div>
+    </div>
+
   return (
-    <div className={'user-container'}>User</div>
+    <div className={'user-container'}>
+      <div className={'user-title'}>
+        <h1>Usuarios</h1>
+      </div>
+      <div className={'user-content'}>
+        <Crud
+          title={'Usuarios'}
+          object={'User'}
+          columns={[
+            {
+              name: 'Id User',
+              key: 'userId',
+              type: 'text'
+            },
+            {
+              name: 'Nombre',
+              key: 'name',
+              type: 'text'
+            },
+            {
+              name: 'Apellido',
+              key: 'lastName',
+              type: 'text'
+            },
+            {
+              name: 'Correo',
+              key: 'email',
+              type: 'text'
+            },
+            {
+              name: 'Carrera',
+              key: 'career',
+              type: 'text'
+            },
+            {
+              name: 'Rol',
+              key: 'role',
+              type: 'text'
+            },
+            {
+              name: 'Género',
+              key: 'gender',
+              type: 'text'
+            }
+          ]}
+          form={form}
+
+        />
+      </div>
+
+    </div>
   )
 }
 
