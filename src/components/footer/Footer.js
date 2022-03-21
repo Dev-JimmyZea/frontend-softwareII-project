4648e73e173d9a405055714f30a6b57d9898a96f
JimmyZea <jimmy.zea@uptc.edu.co>
2022-03-21 12:30:21 -0500

    feat(Root): Change footer structure and add styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,33 +1,287 @@
-import './footer.css'
-
+import "./footer.css";
 
 const Footer = () => {
     return (
-        <div className={'footer-container'}>
-            <div className={''}>
-                <div className={''}>
-                    <div className={''}>
-                        <a href="./"><img  alt="logo" /></a>
+        <footer className="footer-container">
+            <div className="footer-container-first-column">
+                <div className="element piece full only-body no-visual">
+                    <div className="body default">
+                        <div className="text">
+                            <p>
+                                <strong className="display-block">
+                                    UNIVERSIDAD PEDAGÓGICA Y TECNOLÓGICA DE COLOMBIA
+                                </strong>
+                                <span className="display-block-one">
+                                    Sede Central Tunja–Boyacá–Colombia Avenida Central del Norte 39-115
+                                </span>
+
+                                <strong>PBX:</strong> (57+8) 7405626
+                                <a
+                                    className="enlaceAzul"
+                                    href="mailto:portalweb@uptc.edu.co"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">
+                                        portalweb@uptc.edu.co
+                                    </span>
+                                </a>
+                                <a
+                                    className="enlaceAzul"
+                                    title="Horario de atención Sedes UPTC"
+                                    href="http://www.uptc.edu.co/enlaces/portal/horario"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">
+
+                                        Horario de atención y servicio telefónico
+                                    </span>
+                                </a>
+                                Lunes a viernes de 8:00 a.m. a 12:00 m y 2:00 p.m a 6:00 p.m.
+                                <a
+                                    className="enlaceAzul"
+                                    title="Transparencia y Acceso a la Información Pública"
+                                    href="http://www.uptc.edu.co/gel/transparencia"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">
+
+                                        Transparencia y Acceso a la Información Pública
+                                    </span>
+                                </a>
+
+                                <a
+                                    className="enlaceAzul"
+                                    title="Estatutos, valores de matrícula y derechos pecuniarios"
+                                    href="http://www.uptc.edu.co/enlaces/portal/pecuniarios"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">
+                                        Estatutos, valores de matrícula y derechos pecuniarios
+                                    </span>
+                                </a>
+
+                            </p>
+                        </div>
                     </div>
-                    <div className={''}>
-                        <p>Copyright © 2021 <a href="./">Themefisher</a>. All Rights Reserved.</p>
-                    </div>
-                </div>
-                <div className={''}>
-                    <ul className="footer-menu">
-                        <li><a href="./">Home</a></li>
-                        <li><a href="./">About</a></li>
-                        <li><a href="./">Services</a></li>
-                        <li><a href="./">Pricing</a></li>
-                        <li><a href="./">Blog</a></li>
-                        <li><a href="https://wa.me/573219628245" target={'_blank'} rel='noreferrer'>Whatsapp</a></li>
-                        
-                    </ul>
                 </div>
             </div>
-        </div>
+            <div className="footer-container-second-column">
+                <div className="element piece full only-body no-visual">
+                    <div className="body default">
+                        <div className="text">
+                            <p>
+                                <strong className="display-block">INSTITUCIONAL</strong>
+                            </p>
+                            <p>
+                                <a
+                                    href="/sitio/portal/cal_not_eve/cal_not/index.html"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">Comunicaciones</span>
+                                </a>
+                                <a
+                                    href="/sitio/portal/sitios/directorio/index.html"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">Directivos y Oficinas</span>
+                                </a>
+                                <a
+                                    href="/sitio/portal/sitios/universidad/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">Información General</span>
+                                </a>
+                                <a
+                                    href="/sitio/portal/sitios/universidad/rectoria/planeacion/rdc/2022/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">Rendición de Cuentas</span>
+                                </a>
+                                <a
+                                    href="/sitio/portal/sitios/localizacion/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">Sedes</span>
+                                </a>
+                                <a
+                                    href="http://www.uptc.edu.co/infantil/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <span className="display-block-one">UPTC Niños y Niñas</span>
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-container-third-column">
+                <div className="element piece full only-body no-visual">
+                    <div className="body default">
+                        <div className="text">
+                            <p>
+                                <strong>
+                                    <a
+                                        title="Atención al ciudadano"
+                                        href="http://www.uptc.edu.co/enlaces/portal/pqrs"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Atención al Ciudadano
+                                    </a>
+                                </strong>{" "}
 
-    )
-}
+                                <strong>
+                                    <a
+                                        title="Sistema de peticiones, quejas, reclamos y sugerencias"
+                                        href="http://www.uptc.edu.co/sqrs/index.html"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        SPQRS
+                                    </a>
+                                </strong>
+                                <span className="display-block-one">
+                                    <strong>Línea Gratuita:&nbsp;</strong>
+                                    01 8000 942024 <strong>Tel:</strong> (57+8) 7428263
+                                </span>
+                                <span className="display-block-one">
+                                    <strong>Teléfono: </strong>(57+8) 7405626
+                                    <strong>&nbsp; extensión</strong> 2580
+                                </span>
+                                <a href="mailto:quejas.reclamos@uptc.edu.co">
+                                    quejas.reclamos@uptc.edu.co
+                                </a>
 
-export default Footer
+                            </p>
+                            <p className="mt-2">
+                                <em>
+                                    <strong >Notificaciones:</strong>
+                                </em>
+                            </p>
+                            <ul>
+                                <li>
+                                    <a
+                                        className="enlaceAzul"
+                                        title="Notificaciones de aviso"
+                                        href="http://www.uptc.edu.co/enlaces/portal/not_aviso"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Aviso
+                                    </a>
+
+                                </li>
+                                <li>
+                                    <a
+                                        className="enlaceAzul"
+                                        title="Correo para envio de notificaciones judiciales"
+                                        href="http://www.uptc.edu.co/enlaces/portal/njudiciales"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        Judiciales
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        title="Notificaciones Control Interno Disciplinario"
+                                        href="/sitio/portal/sitios/universidad/rectoria/cont_inter_disciplin/index.html"
+                                    >
+                                        Dirección Control Interno Disciplinario
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-container-last-column">
+                <div className="element piece full only-body no-visual">
+                    <div className="body default">
+                        <div className="text">
+                            <p> </p>
+                            <p>
+                                <strong className="display-block">
+                                    Institución de Educación Superior sujeta a inspección y
+                                    vigilancia por el Ministerio de Educación Nacional
+                                </strong>
+                                UPTC - Departamento de Innovación Académica - 2022
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container-icons">
+                    <div>
+                        <ul className="social-icons">
+                            <li className="facebook">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    title="UPTC en Facebook"
+                                    href="https://www.facebook.com/universidaduptc"
+                                >
+                                    <div>
+                                        <i className="fab fa-facebook-f"></i>
+                                    </div>
+                                    <span className="sr-only">UPTC en Facebook</span>
+                                </a>
+                            </li>
+                            <li className="twitter">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    title="UPTC en Twitter"
+                                    href="https://twitter.com/universidaduptc"
+                                >
+                                    <div>
+                                        <i className="fab fa-twitter"></i>
+                                    </div>
+                                    <span className="sr-only">UPTC en Twitter</span>
+                                </a>
+                            </li>
+                            <li className="instagram">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    title="UPTC en Instagram"
+                                    href="https://www.instagram.com/universidaduptc/"
+                                >
+                                    <div>
+                                        <i className="fab fa-instagram"></i>
+                                    </div>
+                                    <span className="sr-only">UPTC en Instagram</span>
+                                </a>
+                            </li>
+                            <li className="youtube">
+                                <a
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    title="UPTC en YouTube"
+                                    href="https://www.youtube.com/channel/UC2Msd-vi6PsH_6q3JJKcAOw"
+                                >
+                                    <div>
+                                        <i className="fab fa-youtube"></i>
+                                    </div>
+                                    <span className="sr-only">UPTC en YouTube</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
